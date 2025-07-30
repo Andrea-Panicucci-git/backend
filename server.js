ab2bffa8c4210e37819365e32a45042ed547bcf5
Andrea-Panicucci-git <andreapanicucci00@gmail.com>
2025-07-30 17:45:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.get('/contatti', async (req, res) => {
   try {
     const [rows] = await pool.query(
       'SELECT * FROM contatti WHERE nome LIKE ? OR email LIKE ?',
-      ['%${search}%', '%${search}%']
+      [`%${search}%`, `%${search}%`]
+
     );
 
     res.json(rows);
